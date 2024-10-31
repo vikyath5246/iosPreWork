bd5de814be246ddaf919117bc93a42365bdbb15c
vikyath <vikyath.komatireddy@oracle.com>
2024-10-31 11:34:56 -0400

    Initial commit

--- prework/ViewController.swift
+++ prework/ViewController.swift
@@ -9,11 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var changeBackgroundColor: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
+        //changeBackgroundColor.setTitle("Press Me", for: .normal)
         // Do any additional setup after loading the view.
     }
+    
+    func changeColor() -> UIColor{
 
+            let red = CGFloat.random(in: 0...1)
+            let green = CGFloat.random(in: 0...1)
+            let blue = CGFloat.random(in: 0...1)
 
+            return UIColor(red: red, green: green, blue: blue, alpha: 0.5)
+        }
+
+    @IBAction func changeBackgroundColorTapped(_ sender: Any) {
+        let randomColor = changeColor()
+        view.backgroundColor = randomColor
+        
+    }
+    
 }
 
